99cf3697a9f60b368572917a49cd7a4ed8e40406
Dro30 <drastik54@gmail.com>
2026-09-16 18:00:46 +0000

    update

--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -1,9 +1,29 @@
 /*
     Lecture note example - Input!!
 */
+import java.util.Scanner;
 
-class LectureInput{
+class LectureInput {
     public static void main(String args[]) {
         // Your Code Goes here!
-	}
+    System.out.println("Bari jalust em resturante");
+    System.out.println("Esa mer untelike:");
+    System.out.println("1: Karmer plav - ");
+    double item1Price = 7.85;
+    System.out.println(item1Price);
+ 
+    System.out.println("2: Shes kabob - ");
+    double item2Price = 12.50;
+   System.out.println(item2Price);
+    
+    System.out.println("3: Lamb chop - ");
+   double item3Price = 15.12;
+   System.out.println(item3Price);
+    
+    
+    
+    
+    
+    
+    }
 }
