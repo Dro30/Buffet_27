1ee5cd66ed351748fe2edba08815b41abb41c57c
Dro30 <drastik54@gmail.com>
2026-09-21 19:14:08 +0000

    update

--- Java/000_Labs/008_input/base_code/starter.java
+++ Java/000_Labs/008_input/base_code/starter.java
@@ -7,8 +7,16 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println("What is your name?");
+		String pic = sc.nextLine();
+		System.out.println("What is your age?");
+		String line = sc.nextLine();
+		System.out.print("What month were you born?"); 
+		String small = sc.nextLine();
+
+
 	}
 }
--- Java/102_Exercises/10_RandomRange/base_code/starter.java
+++ Java/102_Exercises/10_RandomRange/base_code/starter.java
@@ -8,8 +8,25 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 2 numbers to create a range for your random number");
+		System.out.println("Please enter an integer:");
+		int num = sc.nextInt();
+		System.out.println("Please enter another integer (bigger than the first):");
+		int cv = sc.nextInt();
+		System.out.println("Your range is " + num + " to " + cv);
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.print("I love to learn coding remotely.");
+
+
+
 	}
 }
